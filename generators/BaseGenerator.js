5345fb9abaa8a491084c20a03bde55dd50b5fbf3
Andre Guilherme <andre.guilherme@iatec.com>
2017-11-09 11:35:19 -0200

    Create factory for generators

--- generators/BaseGenerator.js
+++ generators/BaseGenerator.js
@@ -60,4 +60,14 @@ class BaseGenerator {
     });
   }
 }
+BaseGenerator.createGenerator = function(format) {
+  if (format in CONFIG.generators) {
+      console.log('Found generator for', format);
+      var genclass = require(`../generators/${CONFIG.generators[format]}`);
+  } else {
+      console.log('Using default generator')
+      var genclass = require(`../generators/${CONFIG.defaultGenerator}`);
+  }
+  return new genclass(format);
+}
 module.exports = BaseGenerator;
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const CONFIG = require('./config.json');
+const BaseGenerator = require('./generators/BaseGenerator');
+
 const program = require('commander');
 
 program
@@ -11,15 +13,8 @@ let args = process.argv.slice(2);
 if (args.length <=0) {
     console.log('Missing FORMAT argument');
 } else {
-    let format = args[0].toLowerCase();
-    if (format in CONFIG.generators) {
-        console.log('Found generator for', format);
-        var genclass = require(`./generators/${CONFIG.generators[format]}`);
-    } else {
-        console.log('Using default generator')
-        var genclass = require(`./generators/${CONFIG.defaultGenerator}`);
-    }
-    let generator = new genclass(format);
+    let format = args[0];
+    let generator = BaseGenerator.createGenerator(format);
     console.log(generator);
     generator.generate()
         .catch(err=>console.error(err))
